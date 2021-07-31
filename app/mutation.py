b4cd7dfa917346309062e5bd3747e3dff85c22df
gyeseok <gyskjng@gmail.com>
2021-07-31 19:22:59 +0900

    feat: add password in UserInput

--- app/mutation.py
+++ app/mutation.py
@@ -5,17 +5,17 @@ from schema import UserSchema
 
 class UserInput(graphene.InputObjectType):
     name = graphene.String(required=True)
+    password = graphene.String(required=True)
 
 
 class CreateUser(graphene.Mutation):
     class Arguments:
-        user_input = UserInput(required=True)
+        input = UserInput(required=True)
 
     user = graphene.Field(UserSchema)
 
     def mutate(root, info, input: UserInput):
-        new_user = db.user.insert_one(
-            {"name": input.name})
+        new_user = db.user.insert_one(input)
         created_user = db.user.find_one({"_id": new_user.inserted_id})
         return CreateUser(user=created_user)
 
